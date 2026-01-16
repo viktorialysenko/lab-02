71f2c9f157b0fab61a7d04fcb63d5ad034ffa44b
Viktoria <112983433+viktorialysenko@users.noreply.github.com>
2026-01-16 15:36:58 -0700

    Participation Exercise

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.listycity;
 
+import static com.google.android.material.internal.ViewUtils.hideKeyboard;
+
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -20,12 +25,17 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
 
+    Button add_city, delete_city, confirm;
+    EditText city_input;
+
+    int selectedPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.app_name);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -40,5 +50,51 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        //Buttons
+        add_city = findViewById(R.id.add_city);
+        delete_city = findViewById(R.id.delete_city);
+        confirm = findViewById(R.id.confirm);
+        city_input = findViewById(R.id.city_input);
+
+        //hidden
+        city_input.setVisibility(View.GONE);
+        confirm.setVisibility(View.GONE);
+
+        //delete is not working until something is selected
+        delete_city.setEnabled(false);
+        //select item for deletion
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+            delete_city.setEnabled(true);
+        });
+
+        //show input
+        add_city.setOnClickListener(v -> {
+            city_input.setVisibility(View.VISIBLE);
+            confirm.setVisibility(View.VISIBLE);
+            city_input.requestFocus();
+        });
+        //ADD city
+        confirm.setOnClickListener(v -> {
+            String cityName = city_input.getText().toString();
+            if (!cityName.isEmpty()) {
+                dataList.add(cityName);
+                cityAdapter.notifyDataSetChanged();//refresh the listview
+            }
+            city_input.setText("");
+            city_input.setVisibility(View.GONE);
+            confirm.setVisibility(View.GONE);
+        });
+
+        //DELETE city
+        delete_city.setOnClickListener(v -> {
+            if (selectedPosition != -1) {
+                dataList.remove(selectedPosition);
+                cityAdapter.notifyDataSetChanged();
+                selectedPosition = -1;
+                delete_city.setEnabled(false);
+            }
+        });
     }
 }
